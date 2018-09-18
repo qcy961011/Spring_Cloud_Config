0dc7dd8c59cd029f1b2931caec18e4133d7c5bc2
Qiao-PC <qcy961011>
2018-09-18 21:25:13 +0800

    新增结合RobbitMQ，暂时未能实现

--- bus-config-client/src/main/java/com/qiao/busconfigclient/BusConfigClientApplication.java
+++ bus-config-client/src/main/java/com/qiao/busconfigclient/BusConfigClientApplication.java
@@ -1,6 +1,5 @@
 package com.qiao.busconfigclient;
 
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
